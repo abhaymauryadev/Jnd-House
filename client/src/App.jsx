1b535d2ea01c7fd24174a154971407112751e8ba
Abhay <abhaymaurya821@gmail.com>
2025-08-26 17:48:17 +0530

    init

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ import Navbar from './components/Navbar';
 import Hero from './pages/Hero';
 import Event from './pages/Event';
 import Footer from './components/Footer';
-import Cursor from './components/cursor';
+import Cursor from './components/Cursor';
 import Aboutus from './pages/Aboutus';
 
 function App() {
